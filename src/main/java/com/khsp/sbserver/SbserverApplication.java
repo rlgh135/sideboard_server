40928da0fcf59670c5c2faf9a3c74e9248823638
rlgh135 <rlgh135@gmail.com>
2025-12-02 10:19:36 +0900

    init

---
+++ src/main/java/com/khsp/sbserver/SbserverApplication.java
@@ -0,0 +1,13 @@
+package com.khsp.sbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khsp/sbserver/SbserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khsp.sbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
